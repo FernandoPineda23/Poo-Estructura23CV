cee310947586f7b79b850689cc44ec4c3929f80e
fer39 <fer39@CHOCONICE>
2023-05-24 16:25:33 -0500

    Agregar archivos de proyecto.

---
+++ Poo-Estructura/Entities/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Estructura.Entities
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string apellido { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public int Grado { get; set; }
+        public double Promedio { get; set; }
+         
+    }
+}
---
+++ Poo-Estructura/Services/AlumnoService.cs
@@ -0,0 +1,35 @@
+﻿using Poo_Estructura.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Estructura.Services
+{
+    public class AlumnoService
+    {
+        public Alumno Agregar()
+        {
+            try
+            {
+                Alumno alumno = new Alumno()
+                {
+                    Id = 1,
+                    Name = "Fernando",
+                    apellido = "Pineda",
+                    FechaIngreso = DateTime.Now,
+                    Grado = 3,
+                    Promedio = 9.5,
+                };
+                return alumno;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("sucedio un error" + ex.Message);
+            }
+            
+        }
+        public bool ValidarProm
+    }
+}
